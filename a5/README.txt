b45ef73609dae8b501c5676698df066aaa0fa10b
zibinmei <zibinmei@outlook.com>
2017-11-27 13:09:34 -0700

    hi

--- a5/README.txt
+++
@@ -1,6 +0,0 @@
-Zibin Mei
-10085722
-
-How to run:
-
-python3 fw.py [rule.txt]
--- a6/bot.py
+++ a6/bot.py
@@ -1,15 +1,15 @@
 import sys
 import socket
 import random
-
+import time
 
 # argv check
 try:
     hostname= sys.argv[1]
-    port = sys.argv[2]
+    port = int(sys.argv[2])
     channel = sys.argv[3]
     sphrase = sys.argv[4]
-except:
+except Exception as err:
     print("invalid arguments: python3 bot.py <hostname> <port> <channel> <secret-phrase>")
     sys.exit()
 
@@ -17,23 +17,60 @@ except:
 
 
 def connect():
-    botnum = str(random.random*99999)
-    irc.connect((hostname, port))
-    msg = "NICK bot" + botnum
-    irc.send(msg.encode())
-    msg = "USER bot * * : bot " + botnum
+    while True:
+        botnum = str(random.randint(0,999999))
+        irc.connect((hostname, port))
+        msg = "NICK bot" + botnum + "\r\n"
+        irc.send(msg.encode())
+        msg = "USER bot * * : bot " + botnum +"\r\n"
+        irc.send(msg.encode())
+        indata = irc.recv(1024)
+        inmsg = indata.decode("utf-8").split("\r\n")
+        state_code=inmsg[0].split()[1]
+        # wait 5s before retrying to coneect
+        if state_code != "001":
+            time.sleep(5)
+            continue
+        else:
+            if joinChannel(channel):
+                break
+    return
+
+def joinChannel(name):
+    # join channel
+    msg = "JOIN #" +name+" \r\n"
     irc.send(msg.encode())
     indata = irc.recv(1024)
-    print(indata)
-    return
-
-def ping:
-    msg = "PING"
+    inmsg = indata.decode("utf-8").split("\r\n")
+    for s in inmsg:
+        print(s)
+    if inmsg[0].split()[1] == "JOIN":
+        print("connected")
+        return True
+    return False
+#handles Ping msg
+def ping(pingmsg):
+    msg = "PONG "+pingmsg+"\r\n"
     irc.send(msg.encode())
     return
 
 
+# use the listen to chat
+def listen():
+    while True:
+        indata = irc.recv(1024)
+        inmsg = indata.decode("utf-8").split()
+        print(inmsg)
+
+        if inmsg[0] == sphrase:
+            print ("serving master!")
+        else:
+            pass
+    return
 
 # main=========================================================================
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+irc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+connect()
+listen()
