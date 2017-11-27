7e6734eef3025eafb22828f709a9bcf9cd470ea5
zibinmei <zibinmei@outlook.com>
2017-11-27 00:24:48 -0700

    working on connect

--- a6/bot.py
+++ a6/bot.py
@@ -1,6 +1,6 @@
 import sys
 import socket
-
+import random
 
 
 # argv check
@@ -13,8 +13,27 @@ except:
     print("invalid arguments: python3 bot.py <hostname> <port> <channel> <secret-phrase>")
     sys.exit()
 
-def connect:
-    global irc
-    irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+
+def connect():
+    botnum = str(random.random*99999)
     irc.connect((hostname, port))
+    msg = "NICK bot" + botnum
+    irc.send(msg.encode())
+    msg = "USER bot * * : bot " + botnum
+    irc.send(msg.encode())
+    indata = irc.recv(1024)
+    print(indata)
     return
+
+def ping:
+    msg = "PING"
+    irc.send(msg.encode())
+    return
+
+
+
+# main=========================================================================
+
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
